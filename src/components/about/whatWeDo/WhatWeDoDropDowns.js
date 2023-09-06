64e46ee2e379ea878a44a22e069eaeaf3c863924
alliehkw <alliesoldau@gmail.com>
2023-09-06 19:33:38 -0400

    added in animation to hide/show the drop down contents in the What We Do page

--- src/components/about/whatWeDo/WhatWeDoDropDowns.js
+++ src/components/about/whatWeDo/WhatWeDoDropDowns.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import DownCarrot from "../../svgs/DownCarrot.js";
 
 function WhatWeDoDropDowns({ what_we_do_data, drop_downs_data }) {
@@ -5,41 +6,75 @@ function WhatWeDoDropDowns({ what_we_do_data, drop_downs_data }) {
   let orthoElements = [];
   let therapyElements = [];
 
+  // State variables to track visibility for each element
+  const [dropdownsVisibility, setDropdownsVisibility] = useState(
+    drop_downs_data.map(() => false)
+  );
+
+  // Function to toggle visibility of dropdown at a specific index
+  const toggleDropdown = (index) => {
+    const updatedVisibility = [...dropdownsVisibility];
+    updatedVisibility[index] = !updatedVisibility[index];
+    setDropdownsVisibility(updatedVisibility);
+  };
+
   // Loop through the data array and populate the variables
   drop_downs_data.forEach((item, index) => {
-    console.log(item.dropDownContent.length);
     if (item.dropDownCategory[0] === "therapy") {
       therapyElements.push(
         <div key={index} className="dropdown therapy">
-          <div className="title-with-carrot">
+          <div
+            className="title-with-carrot"
+            onClick={() => toggleDropdown(index)}
+          >
             <p className="allCaps dd">{item.dropDownTitle}</p>
             <div className="carrot">
               <DownCarrot className="down-carrot" svgColor={"#112E3D"} />
             </div>
           </div>
-          <p
-            className="richText body-text"
-            dangerouslySetInnerHTML={{
-              __html: item.dropDownContent,
-            }}
-          ></p>
+          <div
+            className={`hide-show ${
+              dropdownsVisibility[index] ? "active" : ""
+            }`}
+          >
+            <p
+              className="richText body-text"
+              dangerouslySetInnerHTML={{
+                __html: item.dropDownContent,
+              }}
+            ></p>
+          </div>
         </div>
       );
     } else if (item.dropDownCategory[0] === "orthoCare") {
       orthoElements.push(
         <div key={index} className="dropdown ortho">
-          <div className="title-with-carrot">
+          <div
+            className="title-with-carrot"
+            onClick={() => toggleDropdown(index)}
+          >
             <p className="allCaps dd">{item.dropDownTitle}</p>
             <div className="carrot">
-              <DownCarrot className="down-carrot" svgColor={"#112E3D"} />
+              <DownCarrot
+                className={`down-carrot ${
+                  dropdownsVisibility[index] ? "active" : ""
+                }`}
+                svgColor={"#112E3D"}
+              />
             </div>
           </div>
-          <p
-            className="richText body-text"
-            dangerouslySetInnerHTML={{
-              __html: item.dropDownContent,
-            }}
-          ></p>
+          <div
+            className={`hide-show ${
+              dropdownsVisibility[index] ? "active" : ""
+            }`}
+          >
+            <p
+              className="richText body-text"
+              dangerouslySetInnerHTML={{
+                __html: item.dropDownContent,
+              }}
+            ></p>
+          </div>
         </div>
       );
     }
@@ -57,12 +92,14 @@ function WhatWeDoDropDowns({ what_we_do_data, drop_downs_data }) {
       </div>
       <div className="section">
         <h3 className="section-title">{what_we_do_data.researchTitle}</h3>
-        <p
-          className="richText body-text"
-          dangerouslySetInnerHTML={{
-            __html: what_we_do_data.researchContent,
-          }}
-        ></p>
+        <div className="hide-show">
+          <p
+            className="richText body-text"
+            dangerouslySetInnerHTML={{
+              __html: what_we_do_data.researchContent,
+            }}
+          ></p>
+        </div>
       </div>
     </div>
   );
